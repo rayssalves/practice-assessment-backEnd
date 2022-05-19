08225ee1981d4d25e03f3ea4410478dcd4bc6548
rayssalves <rayssa.a.vieira@outlook.com>
2022-05-19 18:14:14 +0200

    refactoring

--- routers/spaces.js
+++ routers/spaces.js
@@ -14,19 +14,34 @@ router.get("/", async (req, res, next) => {
 
 //goes to the details page
 
-router.get("/:id", async (req, res, next) => {
+// router.get("/:id", async (req, res, next) => {
+//   try {
+//     const specificSpace = await Space.findByPk(req.params.id, {
+//       include: [Story],
+//     });
+//     if (!specificSpace) {
+//       res.status(404).send(`Space with id ${req.params.id} not found`);
+//     } else {
+//       res.send(specificSpace);
+//     }
+//   } catch (e) {
+//     console.log(e);
+//     next(e);
+//   }
+// });
+
+router.get("/:id", async (req, res) => {
   try {
-    const specificSpace = await Space.findByPk(req.params.id, {
-      include: [Story],
+    const spaceId = req.params.id;
+    const getSpaceById = await Space.findByPk(spaceId, {
+      include: [{ model: Story }],
+      //sorting
+      order: [[Story, "createdAt", "DESC"]],
     });
-    if (!specificSpace) {
-      res.status(404).send(`Space with id ${req.params.id} not found`);
-    } else {
-      res.send(specificSpace);
-    }
-  } catch (e) {
-    console.log(e);
-    next(e);
+    res.status(200).send(getSpaceById);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(400).send("invalid ID");
   }
 });
 
